e8eb3b78a0ddce1b5c9867cb17db2274d4d8b727
Olga Shapoval <oshapoval@lbl.gov>
2026-09-15 16:58:11 -0700

    Fix SoA position access for RZ and radial geometries

--- Source/Particles/Resampling/ParticleSplitting.cpp
+++ Source/Particles/Resampling/ParticleSplitting.cpp
@@ -175,8 +175,10 @@ void ParticleSplitting::operator() (
 
     auto& soa = ptile.GetStructOfArrays();
 
-#if !defined(WARPX_DIM_1D_Z)
+#if defined(WARPX_DIM_XZ) || defined(WARPX_DIM_3D)
     auto * const AMREX_RESTRICT x = soa.GetRealData(PIdx::x).data();
+#elif defined(WARPX_DIM_RZ) || defined(WARPX_DIM_RCYLINDER) || defined(WARPX_DIM_RSPHERE)
+    auto * const AMREX_RESTRICT x = soa.GetRealData(PIdx::r).data();
 #endif
 #if defined(WARPX_DIM_3D)
     auto * const AMREX_RESTRICT y = soa.GetRealData(PIdx::y).data();
